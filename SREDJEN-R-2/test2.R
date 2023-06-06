a63bdda1b1a610df2868efc18c7e1c521d13b9e2
VukoticMarko <50705538+VukoticMarko@users.noreply.github.com>
2023-06-06 02:51:34 +0200

    Update test2.R

--- SREDJEN-R-2/test2.R
+++ SREDJEN-R-2/test2.R
@@ -5,7 +5,7 @@
 #b) 18.443%
 #c) H0(p=0.15)
 #d) parametarskih
-#e) 0.1506, prihvata
+#e) 0.1506, prihvatamo, slican je p ko u tekstu zadatka
 
 #f <- file.choose(), pa read.csv(f)
 tips <- read.csv("tips.csv")
@@ -19,16 +19,16 @@ binom.test(K, n, 0.15)
 #b) X i Y su nezavisna
 #c) 9.8302
 #d) Hi-kvadtat raspodela reda 0.95, stepen slobode 2
-#e) 0.007335, ne prihvata
+#e) 0.007335, ne prihvatamo?
 
 chisq.test(matrix(c(11, 21, 17, 8, 29, 14), ncol = 3))
 
 #3.
 #a) H0(m1=m2=m3=m4) - srednje vrednosti racuna po danima jednake
 #b) tips$total_bill - racun
-#c) F (Fiserova), 3, 240
+#c) F (Fiserova) dF, 3, 240
 #d) 2.7675
-#e) 0.04245, ne prihvata
+#e) 0.04245, ne prihvatamo?
 
 boxplot(total_bill~day, data = tips)
 anova(lm(total_bill~day, data = tips))
