87a9450ecf79a455ae00493ced299f0160762bf8
Aleksandar Jovanović <50550669+JovanovicAca@users.noreply.github.com>
2023-06-06 21:28:53 +0200

    Update test2.2.R

--- SREDJEN-R-2/test2.2.R
+++ SREDJEN-R-2/test2.2.R
@@ -1,32 +1,39 @@
 # Test 2, Gr.B
 
 #1
-#a) parametar p binomne raspodele
+#a) U zadatku se testira hipoteza o srednjoj vrednosti napojnica koje su muškarci ostavljali.
 #b) 3.089618
-#c) H0(p?) NE MOGU P skontati....
+#c) Nulta hipoteza ovog testa glasi da je srednja vrednost napojnica koje su muškarci ostavljali manja ili jednaka 3.5 dolara.
 #d) parametarskih
-#e) Ako p skontam onda moze binom.test proci
+#e) 0.9996, prihvatiti(valjda)
 
-tips <- read.csv("tips.csv")
-nap <- tips$tip
-mean(nap[tips$sex=="Male"])
-n <- length(nap)
-K<-length(nap[tips$sex=="Male"])
-binom.test(K, n, p?)
+tips <- read.csv("F:/tips.csv")
+nap <- tips$tip[tips$sex == "Male"]
+mean_nap <- mean(nap)
+t.test(nap, mu = 3.5, alternative = "greater")
 
 #2. 
 #a) neparametarski
-#b) H0(totalbill i dnorm())?
-#c) ks.test(
+#b) Empirijska distribucija podataka odgovara normalnoj raspodeli N(20, 9)
+#c) 1.994
+#d) 0.1276, 1.36 ( ako je lambda 0.99 onda je 1.63)
+#e) 0.00070, ne prihvata (P<0.05)
 
+racun<-tips$total_bill
+ks_test <- ks.test(racun, "pnorm", mean = 20, sd = 9)
+c) dn<-ks_test$statistic * sqrt(length(racun))
+d) realizovana_vrednost <- ks_test$statistic
+e) p_vrednost <- ks_test$p.value
 
 #3.
-#a) H0(m1=m2=m3=m4) - srednje vrednosti visine napojnica jednake
-#b) nivoi faktora?
-#c) F (Fiserova), 3, 240
+#a) H0(m1=m2=m3=m4) - srednje vrednosti visine napojnica po danima su jednake
+#b) Nivoi faktora po kojima se radi ANOVA su dani (day - Sun, Sat, Thur, Fri).
+#c) F (Fiserova), 3
 #d) 1.6724
-#e) 0.1736, 
+#e) 0.1736, ne postoje (DA LI JE NULTA HIP. PRIHVACENA(P>0.05) -> JESTE ONDA NE POSTOJE RAZLIKE)
 
+
+tip <- tips$tip
 boxplot(tip~day, data = tips)
 anova(lm(tip~day, data = tips))
 
